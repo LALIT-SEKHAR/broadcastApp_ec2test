74232f2b8b55e26a732b0fab4fcf670f566e4641
LALIT-SEKHAR <lalitsekhar1999@gmail.com>
2021-09-10 11:48:07 +0530

    make it better

--- src/ConnectPeer/PeerConnect.js
+++ src/ConnectPeer/PeerConnect.js
@@ -14,10 +14,11 @@ module.exports.createPeer = () => {
 };
 
 module.exports.addTracks = ({ mediaStream, peer }) => {
-  return mediaStream.getTracks().forEach((track) => {
-    console.log(track[0]);
-    peer.addTrack(track, mediaStream);
-  });
+  // return mediaStream.getTracks().forEach((track) => {
+  //   console.log(track[0]);
+  //   peer.addTrack(track, mediaStream);
+  // });
+  return peer.addTrack(mediaStream.getAudioTracks()[0], mediaStream);
 };
 
 module.exports.createOffer = async ({ peer }) => {
