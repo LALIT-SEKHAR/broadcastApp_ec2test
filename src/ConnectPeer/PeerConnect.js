a4464d71690c4ff2f9f0c4e8131fda7b9288f668
LALIT-SEKHAR <lalitsekhar1999@gmail.com>
2021-09-21 15:43:49 +0530

    added own turn/stun sterver

--- src/ConnectPeer/PeerConnect.js
+++ src/ConnectPeer/PeerConnect.js
@@ -3,42 +3,11 @@ const Webrtc = require("wrtc");
 module.exports.createPeer = () => {
   return new Webrtc.RTCPeerConnection({
     iceServers: [
-      { urls: "stun:stun.ekiga.net" },
-      { urls: "stun:stun.schlund.de" },
-      { urls: "stun:stun.l.google.com:19302" },
-      { urls: "stun:stun1.l.google.com:19302" },
-      { urls: "stun:stun2.l.google.com:19302" },
-      { urls: "stun:stun3.l.google.com:19302" },
-      { urls: "stun:stun4.l.google.com:19302" },
-      { urls: "stun:stun.softjoys.com" },
-      { urls: "stun:stun.voipbuster.com" },
-      { urls: "stun:stun.voipstunt.com" },
-      { urls: "stun:stun.xten.com" },
-      { urls: "stun:stun.stunprotocol.org" },
+      { urls: "stun:3.108.84.209:3478" },
       {
-        urls: "turn:numb.viagenie.ca",
-        credential: "muazkh",
-        username: "webrtc@live.com",
-      },
-      {
-        urls: "turn:192.158.29.39:3478?transport=udp",
-        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-        username: "28224511:1379330808",
-      },
-      {
-        urls: "turn:192.158.29.39:3478?transport=tcp",
-        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-        username: "28224511:1379330808",
-      },
-      {
-        urls: "turn:numb.viagenie.ca",
-        credential: "biDzUg8GhjQth8T",
-        username: "lalitasekhar1999@gmail.com",
-      },
-      {
-        urls: "turn:numb.viagenie.ca",
-        credential: "biDzUg8GhjQth8T",
-        username: "lalitsekhar1999@gmail.com",
+        urls: "turn:3.108.84.209:3478",
+        credential: "lalitsekharbehera",
+        username: "area51@donotusethispassword",
       },
     ],
   });
